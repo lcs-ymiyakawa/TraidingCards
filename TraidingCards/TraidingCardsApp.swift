ed52a4e4c763dd35c06eb1bc593cc7523bda40a1
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-01-18 14:09:33 -0500

    Initial Commit

---
+++ TraidingCards/TraidingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraidingCardsApp.swift
+//  TraidingCards
+//
+//  Created by 宮川義之助 on 2024/01/18.
+//
+
+import SwiftUI
+
+@main
+struct TraidingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
